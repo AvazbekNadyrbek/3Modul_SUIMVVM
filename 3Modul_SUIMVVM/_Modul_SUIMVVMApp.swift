d5771c2cb12ac21ec128fae7704e8da922749b66
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2025-07-13 18:33:35 +0200

    Initial Commit

---
+++ 3Modul_SUIMVVM/_Modul_SUIMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  _Modul_SUIMVVMApp.swift
+//  3Modul_SUIMVVM
+//
+//  Created by Авазбек Надырбек уулу on 13.07.25.
+//
+
+import SwiftUI
+
+@main
+struct _Modul_SUIMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
